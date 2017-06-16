e65df0daeb05005ec43705ead560cedcb7766980
Ettore Di Giacinto <edigiacinto@suse.com>
2017-06-16 17:12:58 +0200

    wip: backend::component::dnsserver, forward unknown request to external DNS server

--- backend/component/dnsserver.pm
+++ backend/component/dnsserver.pm
@@ -1,14 +1,21 @@
+
 package backend::component::dnsserver;
 use Mojo::Base -base;
 use Net::DNS::Nameserver;
 use backend::component::dnsresolver;
 use bmwqemu;
 
-has [qw(record_table listening_port listening_address)];
+has [qw(record_table listening_port listening_address )];
+
+has 'forward_nameserver' => sub {['8.8.8.8']};
+
+has 'global_policy' => 'FORWARD';
 
 sub start {
 
     my $self = shift;
+
+    bmwqemu::diag(">> DNS Server Global Policy is ".$self->global_policy());
 
     my $sinkhole = backend::component::dnsresolver->new(%{$self->record_table});
 
@@ -23,8 +30,23 @@ sub start {
 
             if ($self->record_table()->{$qname} and $self->record_table()->{$qname} eq "FORWARD") {
                 $rcode = "SERVFAIL";
-                bmwqemu::diag(">> Forward for $qname , returning $rcode");
-                return ($rcode, []);
+                bmwqemu::diag(">> Forward for $qname");
+
+                my $forward_resolver = new Net::DNS::Resolver(
+                nameservers => $self->forward_nameserver(),
+                recurse     => 1,
+                debug       => 0
+                );
+                my $question = $forward_resolver->query($qname, $qtype, $qclass);
+
+                if (defined $question) {
+                    @ans = $question->answer();
+                    bmwqemu::diag(">> DNS Server: Answer(FWD) " . $_->string) for @ans;
+                    $rcode = "NOERROR";
+                }
+
+                return ($rcode, \@ans);
+
             }
             elsif ($self->record_table()->{$qname} and $self->record_table()->{$qname} eq "DROP") {
                 $rcode = "NXDOMAIN";
@@ -32,16 +54,39 @@ sub start {
                 return ($rcode, []);
             }
 
+
+
             my $question = $sinkhole->query($qname, $qtype, $qclass);
 
             if (defined $question) {
                 @ans = $question->answer();
                 bmwqemu::diag(">> DNS Server: Answer " . $_->string) for @ans;
                 $rcode = "NOERROR";
+            } else {
+                    $rcode = "NXDOMAIN";
+                }
+
+            if(@ans == 0 && $self->global_policy() eq "FORWARD") {
+              bmwqemu::diag(">> DNS Server: Global policy is FORWARD, forwarding request to " . join(", ",@{$self->forward_nameserver()} ) ) ;
+
+              my $forward_resolver = new Net::DNS::Resolver(
+              nameservers => $self->forward_nameserver(),
+              recurse     => 1,
+              debug       => 0
+              );
+              my $question = $forward_resolver->query($qname, $qtype, $qclass);
+
+                if (defined $question) {
+                    @ans = $question->answer();
+                    bmwqemu::diag(">> DNS Server: Answer(FWD-fallback) " . $_->string) for @ans;
+                    $rcode = "NOERROR";
+                }
+                else {
+                    $rcode = "NXDOMAIN";
+                }
+
             }
-            else {
-                $rcode = "NXDOMAIN";
-            }
+
             return ($rcode, \@ans,);
 
         },
