4d10c97f665739c21dc532c4ad420d4f56e5dba9
Ettore Di Giacinto <edigiacinto@suse.com>
2017-06-20 13:48:51 +0200

    backend::component::proxy: make possible to FORWARD also with single redirection rules

--- backend/component/proxy.pm
+++ backend/component/proxy.pm
@@ -33,20 +33,7 @@ sub startup {
 sub _handle_request {
     my ($self, $controller) = @_;
     $controller->render_later;
-    $self->_redirect($controller);
-}
 
-sub _diag {
-    my ($self, @messages) = @_;
-
-    bmwqemu::diag ">> " . __PACKAGE__ . " @messages";
-
-}
-
-sub _redirect {
-    my $self       = shift;
-    my $controller = shift;
-    my $host_entry = $self->redirect_table;
 
     my $r_url     = $controller->tx->req->url;
     my $r_urlpath = $controller->tx->req->url->path;
@@ -63,74 +50,19 @@ sub _redirect {
 
     $self->_diag("Request from: " . $client_address . " method: " . $r_method . " to host: " . $r_host);
     $self->_diag("Requested url is: " . $controller->tx->req->url->to_abs);
-    if ($self->policy eq "DROP") {
-        $self->_diag("Answering with 404");
+
+        if ($self->policy eq "DROP") {
+            $self->_diag("Answering with 404");
+            $controller->reply->not_found;
+            return;
+        }
+
+    my $tx = $self->_build_tx($controller, $r_host, $r_urlpath, $r_method);
+    unless ($tx) {
+        $self->_diag("Proxy was unable to build the request");
         $controller->reply->not_found;
         return;
     }
-
-    #Start forging - FORWARD by default
-    my $tx = Mojo::Transaction::HTTP->new();
-    $tx->req($controller->tx->req->clone());    #this is better, we keep also the same request
-    $tx->req->method($r_method);
-
-    if ($self->policy eq "SOFTREDIRECT") {
-        $controller->reply->not_found and return unless exists $host_entry->{$r_host};
-
-        my @rules = @{$host_entry->{$r_host}};
-
-        my $redirect_to = shift @rules;
-        do { $self->diag("Odd number of rewrite rules given. Expecting even") }
-          and return unless scalar(@rules) % 2 == 0;
-        for (my $i = 0; $i <= $#rules; $i += 2) {
-            my $redirect_replace      = $rules[$i];
-            my $redirect_replace_with = $rules[$i + 1];
-            $r_urlpath =~ s/$redirect_replace/$redirect_replace_with/g;
-        }
-
-        my $url = Mojo::URL->new($redirect_to =~ /http:\/\// ? $redirect_to : "http://" . $redirect_to);
-
-        if ($url) {
-            $tx->req->url->parse("http://" . $url->host);
-            $tx->req->url->base->host($url->host);
-            $tx->req->content->headers->host($url->host);
-        }
-
-        if ($url and $url->path ne "/") {
-            $tx->req->url->path($url->path . $r_urlpath);
-
-        }
-        else {
-            $tx->req->url->path($r_urlpath);
-
-        }
-        $self->_diag("Redirecting to: " . $tx->req->url->to_abs . " Path: " . $tx->req->url->path);
-    }
-    elsif ($self->policy eq "REDIRECT" && exists $host_entry->{$r_host}) {
-        my $redirect_to = @{$host_entry->{$r_host}}[0];
-        my $url = Mojo::URL->new($redirect_to =~ /http:\/\// ? $redirect_to : "http://" . $redirect_to);
-        if ($url) {
-            $tx->req->url->parse("http://" . $url->host);
-            $tx->req->url->base->host($url->host);
-            $tx->req->content->headers->host($url->host);
-        }
-        if ($url and $url->path ne "/") {
-            $tx->req->url->path($url->path . $r_urlpath);
-        }
-        else {
-            $tx->req->url->path($r_urlpath);
-
-        }
-        $self->_diag("Redirecting to: " . $tx->req->url->to_string);
-    }
-    elsif ($self->policy eq "FORWARD" or (($self->policy eq "REDIRECT" && !exists $host_entry->{$r_host})))
-    {    # If policy is REDIRECT and no entry in the host table, fallback to FORWARD
-        $tx->req->url->parse("http://" . $r_host);
-        $tx->req->url->path($r_urlpath);
-
-        $self->_diag("No redirect rules for the host, forwarding to: " . $r_host);
-    }
-
     $tx->req->url->query($controller->tx->req->params);
     my $req_tx = $self->ua->inactivity_timeout(20)->max_redirects(5)->connect_timeout(20)->request_timeout(10)->start($tx);
 
@@ -142,6 +74,102 @@ sub _redirect {
 
     $controller->tx->res($req_tx->res);
     $controller->rendered;
+
+}
+
+sub _diag {
+    my ($self, @messages) = @_;
+
+    bmwqemu::diag ">> " . __PACKAGE__ . " @messages";
+
+}
+
+sub _build_tx {
+  my ($self, $controller,$r_host, $r_urlpath, $r_method) = @_;
+  my $host_entry=$self->redirect_table;
+
+  #Start forging - FORWARD by default
+  my $tx = Mojo::Transaction::HTTP->new();
+  $tx->req($controller->tx->req->clone());    #this is better, we keep also the same request
+  $tx->req->method($r_method);
+
+  if ($self->policy eq "SOFTREDIRECT") {
+      $controller->reply->not_found and return unless exists $host_entry->{$r_host};
+
+      my @rules = @{$host_entry->{$r_host}};
+
+      my $redirect_to = shift @rules;
+      do { $self->diag("Odd number of rewrite rules given. Expecting even") }
+        and return unless scalar(@rules) % 2 == 0;
+      for (my $i = 0; $i <= $#rules; $i += 2) {
+          my $redirect_replace      = $rules[$i];
+          my $redirect_replace_with = $rules[$i + 1];
+          if($redirect_replace_with eq "FORWARD" and $r_urlpath =~ /$redirect_replace/i ) {
+            $tx->req->url->parse("http://" . $r_host);
+            $tx->req->url->path($r_urlpath);
+            $self->_diag("Rewrite rule matches a FORWARD! forwarding request to: ".$tx->req->url->to_abs);
+            return $tx;
+          }
+          $r_urlpath =~ s/$redirect_replace/$redirect_replace_with/g;
+      }
+
+      my $url = Mojo::URL->new($redirect_to =~ /http:\/\// ? $redirect_to : "http://" . $redirect_to);
+
+      if ($url) {
+          $tx->req->url->parse("http://" . $url->host);
+          $tx->req->url->base->host($url->host);
+          $tx->req->content->headers->host($url->host);
+      }
+
+      if ($url and $url->path ne "/") {
+          $tx->req->url->path($url->path . $r_urlpath);
+
+      }
+      else {
+          $tx->req->url->path($r_urlpath);
+
+      }
+      $self->_diag("Redirecting to: " . $tx->req->url->to_abs . " Path: " . $tx->req->url->path);
+  }
+  elsif ($self->policy eq "REDIRECT" && exists $host_entry->{$r_host}) {
+      my $redirect_to = @{$host_entry->{$r_host}}[0];
+      my $url = Mojo::URL->new($redirect_to =~ /http:\/\// ? $redirect_to : "http://" . $redirect_to);
+      if ($url) {
+          $tx->req->url->parse("http://" . $url->host);
+          $tx->req->url->base->host($url->host);
+          $tx->req->content->headers->host($url->host);
+      }
+      if ($url and $url->path ne "/") {
+          $tx->req->url->path($url->path . $r_urlpath);
+      }
+      else {
+          $tx->req->url->path($r_urlpath);
+
+      }
+      $self->_diag("Redirecting to: " . $tx->req->url->to_string);
+  }
+  elsif ($self->policy eq "FORWARD" or (($self->policy eq "REDIRECT" && !exists $host_entry->{$r_host})))
+  {    # If policy is REDIRECT and no entry in the host table, fallback to FORWARD
+      $tx->req->url->parse("http://" . $r_host);
+      $tx->req->url->path($r_urlpath);
+
+      $self->_diag("No redirect rules for the host, forwarding to: " . $r_host);
+  }
+  return $tx;
+}
+
+sub _redirect {
+    my $self       = shift;
+    my $controller = shift;
+    my $host_entry = $self->redirect_table;
+
+
+
+
+
+
+
+
 }
 
 sub start {
